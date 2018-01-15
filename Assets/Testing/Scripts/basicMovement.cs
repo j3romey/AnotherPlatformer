834fad673c9b83361deb908cec8cedbfaac44b24
ajadvien@ucalgary.ca <ajadvien@ucalgary.ca>
2018-01-14 21:34:08 -0700

    starting out new platformer

---
+++ Assets/Testing/Scripts/basicMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class basicMovement : MonoBehaviour {
+
+	Rigidbody2D rg2D;
+
+	// Use this for initialization
+	void Start () {
+		rg2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+}
