3f5d9cd9466c7b9d0a606bb414b75f2248a53807
ajadvien@ucalgary.ca <ajadvien@ucalgary.ca>
2018-01-15 23:03:05 -0700

    got double jump to properly work

--- Assets/Testing/Scripts/platformerCharacter.cs
+++ Assets/Testing/Scripts/platformerCharacter.cs
@@ -13,6 +13,8 @@ public class platformerCharacter : MonoBehaviour {
 	private const float ghostJumpDelay = 0.25f;
 	private float ghostJumpTimer = 0.25f;
 
+
+
 	private const float baseMethodTimer = 0.25f;
 	private float methodTimer = 0.25f;
 	private bool methodTimerRunning = false;
@@ -36,11 +38,11 @@ public class platformerCharacter : MonoBehaviour {
 
 	void FixedUpdate(){
 		if(rg2D.velocity.y < 0) {
-			rg2D.gravityScale = 3.0f;
+			rg2D.gravityScale = 8.0f;
 		}
 
 		if(rg2D.velocity.y >= 0){
-			rg2D.gravityScale = 1.0f;
+			rg2D.gravityScale = 2.0f;
 		}
 	}
 
@@ -58,10 +60,10 @@ public class platformerCharacter : MonoBehaviour {
 		}
 
 
-		// check if grounded and reset variables
+		// check if grounded and reset variables stopping double jump
 		Collider2D[] colliders = Physics2D.OverlapCircleAll (groundCheck.position, groundCheckRadius, groundLayer);
 
-		if (colliders.Length > 0) {
+		if (colliders.Length > 0 && rg2D.velocity.y <= 0) {
 			for (int i = 0; i < colliders.Length; i++) {
 				if (colliders [i].gameObject != gameObject) {
 					ghostJumpTimer = ghostJumpDelay;
@@ -77,13 +79,13 @@ public class platformerCharacter : MonoBehaviour {
 	public void Move(float movement, bool jump){
 		rg2D.velocity = new Vector2(movement*speed, rg2D.velocity.y);
 
-		if (jump && (grounded || ghostJumpTimer > 0) && !methodTimerRunning)  {
-
-			Debug.Log ("Jump 1");
+		if (jump && (grounded || ghostJumpTimer > 0) && !methodTimerRunning) {
+			
+			Debug.Log ("Jump 1");/*
 			Debug.Log ("jump: " + jump);
 			Debug.Log ("ground: " + grounded);
 			Debug.Log ("timer: " + (ghostJumpTimer > 0));
-
+			*/
 
 			grounded = false;
 			ghostJumpTimer = 0;
@@ -92,14 +94,14 @@ public class platformerCharacter : MonoBehaviour {
 			Jump ();
 			methodTimerRunning = true;
 
-		}/*else if(jump && doubleJump){
+		}else if(jump && doubleJump){
 			Debug.Log ("Jump 2");
 			doubleJump = false;
 			Jump ();
 		}
 		Debug.Log ("jump: " + jump);
 		Debug.Log ("double jump: " + doubleJump);
-		Debug.Log ("else if: " + (doubleJump && jump));*/
+		Debug.Log ("else if: " + (doubleJump && jump));
 	}
 
 	public void Jump(){
